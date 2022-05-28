6082b7e3a4825a4361250c4cc28872fea47b8b78
Ganesh Pilli <neghani@gmail.com>
2022-05-28 08:00:20 +0530

    storage added

--- Week6_JS_OOP/app.js
+++ Week6_JS_OOP/app.js
@@ -2,21 +2,29 @@ let todoContainer = document.querySelector("#todos");
 
 class Todo {
   newElem = "";
-  
-  constructor(text, container) {
+
+  constructor(text, container, saved) {
     this.text = text;
     this.container = container;
     this.done = false;
     this.addToView();
+    if (saved==false) {
+      this.saveTheTodo();
+    }
   }
-
+  saveTheTodo() {
+    let existingData = localStorage.getItem("todo");
+    existingData = existingData
+      ? existingData + " |!@ " + this.text
+      : this.text;
+    localStorage.setItem("todo", existingData);
+  }
   addToView() {
     this.newElem = document.createElement("li");
     this.newElem.innerHTML = this.text;
 
     var markDoneBtn = document.createElement("button");
     markDoneBtn.innerHTML = "Done";
-
     var dltDoneBtn = document.createElement("button");
     dltDoneBtn.innerHTML = "X";
     dltDoneBtn.onclick = () => {
@@ -41,9 +49,17 @@ var allTodos = [];
 
 function saveTodo() {
   let todoText = document.querySelector("#newTodo").value;
-  allTodos.push(new Todo(todoText, todoContainer));
+  allTodos.push(new Todo(todoText, todoContainer, false));
 }
 
-function undoDelete(){
-    allTodos[0].addToView()
+function undoDelete() {
+  allTodos[0].addToView();
+}
+
+function startTheApp() {
+  let notesData = localStorage.getItem("todo");
+  notesData = notesData.split(" |!@ ");
+  notesData.forEach((element) => {
+    new Todo(element, todoContainer, true);
+  });
 }
